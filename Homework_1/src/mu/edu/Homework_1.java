91fb79f2248f17339c1ec821009175f6d8576a5e
Connor Joyce <steelcity2003@gmail.com>
2024-01-30 17:01:09 -0600

    added printInfo function

--- Homework_1/src/mu/edu/Homework_1.java
+++ Homework_1/src/mu/edu/Homework_1.java
@@ -2,12 +2,20 @@ package mu.edu;
 
 public class Homework_1 {
 
-	
+	public static void printInfo(String name, String gitHubID) {
+		System.out.println("This group member's name is "+name+" and their GitHub ID is "+gitHubID);
+	}
 
 	public static void main(String[] args) {
 		String connorGitId = "Conj43";
-		System.out.println(connorGitId);
+		String connorName = "Connor Joyce";
+		
+		
+		printInfo(connorName, connorGitId);
 
 	}
+	
+	
+	
 
-}//hello
+}
